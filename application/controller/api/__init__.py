0f7c4ff9a6da9b0032090f4d8a1eb224ced37d2d
jiang yinzuo <392711804@qq.com>
2019-07-26 20:32:15 +0800

    refactor: route /wx_login

--- application/controller/api/__init__.py
+++ application/controller/api/__init__.py
@@ -3,3 +3,6 @@ from flask import Blueprint
 api_blueprint = Blueprint('api_blueprint', __name__)
 
 from . import wx_login
+
+# print(id(api_blueprint))
+# print(id(wx_login.api_blueprint))
--- application/controller/api/wx_login.py
+++ application/controller/api/wx_login.py
@@ -1,8 +1,21 @@
-from flask import request
+from flask import request, jsonify, make_response
+
+from application.utils.wx_login import auth_wx_login
 from . import api_blueprint
 
 
 @api_blueprint.route('/wx_login', methods=['POST'])
 def wx_login():
-    print(request.data)
-    return request.data
+    data = request.get_json()
+    if 'code' not in data:
+        response = make_response({
+            'err_msg': 'argument code is not defined',
+            'err_code': '4001'
+        }, 400)
+        response.mimetype = 'application/json'
+        return response
+    js_code = data['code']
+    response = auth_wx_login(js_code=js_code)
+    print(response)
+    # TODO: save session_key
+    return jsonify(openid=response['openid'])
