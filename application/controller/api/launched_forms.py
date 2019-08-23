3d03e405f15e731b5989aa81f8e5174053e831cc
Jiang Yinzuo <392711804@qq.com>
2019-08-23 23:02:11 +0800

    fix: in launched_forms_models.py: update_one() params error

--- application/controller/api/launched_forms.py
+++ application/controller/api/launched_forms.py
@@ -3,16 +3,19 @@ import json
 from flask import request, make_response, current_app
 from flask.views import MethodView
 from application.models.launched_forms_model import LaunchedFormsModel
+from application.utils.exception.custom_exception import CustomException
 
 
 class LaunchedFormsAPI(MethodView):
     url = '/launched_forms'
 
-    def patch(self):
+    def put(self):
         data: dict = json.loads(request.data)
-
-        launched_forms_model = LaunchedFormsModel(current_app, data['open_id'])
-        launched_forms_model.patchLaunchedForms(data['form_temp_id'], data['date_time'])
+        try:
+            launched_forms_model = LaunchedFormsModel(current_app, data['open_id'])
+            launched_forms_model.put_launched_forms(data['form_temp_id'], data['date_time'])
+        except CustomException as e:
+            return e.make_response(request_str='PATCH /launched_forms')
 
         return make_response({
             'err_code': 0,
--- application/models/launched_forms_model.py
+++ application/models/launched_forms_model.py
@@ -10,18 +10,18 @@ class LaunchedFormsModel:
         self.__current_app = current_app
         self.__person = PersonModel(self.__current_app, open_id)
 
-    def patchLaunchedForms(self, form_temp_id: str, date_time: str):
+    def put_launched_forms(self, form_temp_id: str, date_time: str):
         if date_time == 'now':
             res = self.__current_app.mongo.db.form_templates.update_one({
                 '_id': ObjectId(form_temp_id)
             }, {
-                'type': 'ended'
+                '$set': {'type': 'ended'}
             })
         else:
             res = self.__current_app.mongo.db.form_templates.update_one({
                 '_id': ObjectId(form_temp_id)
             }, {
-                'end_time': date_time
+                '$set': {'end_time': date_time}
             })
 
         if res.matched_count <= 0:
